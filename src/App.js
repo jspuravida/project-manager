5f021388a7634c366a3d39a2390160f1bb13ee88
jspuravida <julian.salari@gmail.com>
2017-03-21 14:27:19 -0700

    Added components to add new projects to the main state. Not persisting yet

--- src/App.js
+++ src/App.js
@@ -1,18 +1,49 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Projects from './Components/Projects';
+import AddProject from './Components/AddProject';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+
+    // Setting the state here, the state is then passed to Projects component as a property. This usually isn't where the data goes, only states and keys. Use lifecycle method instead (component will mount)
+
+    this.state = {
+      projects: []
+    }
+  }
+
+// Below is the lifecycle method. If you are instead fetching data from an outside API, you will also do it in this lifecycle method as well, or in componentDidMount
+
+  componentWillMount(){
+    this.setState({projects: [
+        {
+          title: 'Business Website',
+          category: 'Web Design'
+        },
+        {
+          title: 'Social App',
+          category: 'Mobile Development'
+        },
+        {
+          title: 'Ecommerce Shopping Cart',
+          category: 'Web Development'
+        }
+    ]});
+  }
+
+  handleAddProject(project){
+    let projects = this.state.projects;
+    projects.push(project);
+    this.setState({projects:projects});
+  }
+
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <AddProject addProject={this.handleAddProject.bind(this)} />
+        <Projects projects={this.state.projects} />
       </div>
     );
   }
---
+++ src/Components/AddProject.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+
+class AddProject extends Component {
+    constructor(){
+        super();
+        this.state = {
+            newProject:{}
+        }
+    }
+
+    static defaultProps = {
+        categories: ['Web Design', 'Web Development', 'Mobile Development']
+    }
+
+    handleSubmit(e){
+        if(this.refs.title.value === ''){
+            alert('Title is required');
+        } else {
+            this.setState({newProject:{
+                title: this.refs.title.value,
+                category: this.refs.category.value
+            }}, function(){
+
+                // I am passing along the new project to app.js  
+                this.props.addProject(this.state.newProject);
+            });
+        }
+        e.preventDefault();
+    }
+    // Now we have the data logged, we want to pass it up to the main app component, and save it in that state. We send it up through a property (a function inside the properties). 
+
+  render() {
+      let categoryOptions = this.props.categories.map(category => {
+          return <option key={category} value={category}>{category}</option>
+      });
+ 
+    return (
+      <div>
+        <h3>Add Project</h3>
+        <form onSubmit={this.handleSubmit.bind(this)}>
+            <div>
+                <label>Title</label><br />
+                <input type="text" ref="title" />
+            </div>
+            <div>
+                <label>Category</label><br />
+                <select ref="category">
+                    {categoryOptions}
+                </select>
+            </div>
+            <input type="submit" value="Submit" />
+        </form>
+      </div>
+
+    //   Now to store the data we submit into state, so we use a constructor at the top
+
+    );
+  }
+}
+
+export default AddProject;
