5c4f860b6a36196195caa79d4c6c72216a440ea2
jspuravida <julian.salari@gmail.com>
2017-03-24 12:37:54 -0700

    fetching_data_from_api

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import uuid from 'uuid';
+import $ from 'jquery';
 import Projects from './Components/Projects';
 import AddProject from './Components/AddProject';
 import './App.css';
@@ -11,14 +12,34 @@ class App extends Component {
     // Setting the state here, the state is then passed to Projects component as a property. This usually isn't where the data goes, only states and keys. Use lifecycle method instead (component will mount)
 
     this.state = {
-      projects: []
+      projects: [],
+      todos: []
     }
   }
 
 // Below is the lifecycle method. If you are instead fetching data from an outside API, you will also do it in this lifecycle method as well, or in componentDidMount
 
-  componentWillMount(){
-    this.setState({projects: [
+// Create a function called getTodos, getProjects, then just call them in the componentWill/DidMount. Makes things cleaner.
+
+// Now this will fetch the To Do List. We can do this with modules, but we'll use Jquery.
+getTodos(){
+  $.ajax({
+    url: 'http://jsonplaceholder.typicode.com/todos',
+    dataType: 'json',
+    cache: false,
+    success: function(data){
+      this.setState({todos: data}, function(){
+        console.log(this.state);
+      });
+    }.bind(this),
+    error: function(xhr, status, err){
+      console.log(err);
+    }
+  })
+}
+
+getProjects(){
+  this.setState({projects: [
         {
           id:uuid.v4(),
           title: 'Business Website',
@@ -35,6 +56,17 @@ class App extends Component {
           category: 'Web Development'
         }
     ]});
+}
+
+// Now since we are calling the functions in these lifecycle methods below, we will have the data from the API as soon as the page is loaded.
+
+  componentWillMount(){
+    this.getProjects();
+    this.getTodos();
+  }
+
+  componentDidMount(){
+    this.getTodos();    
   }
 
   handleAddProject(project){
